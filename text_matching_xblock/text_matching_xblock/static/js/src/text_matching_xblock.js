46f00069cecbd14d8dacb2e99749ec3e8385f81f
Dat Tran <trantuandat26@gmail.com>
2023-06-30 09:22:35 +0700

    feat: Serve data from server and handle drop event

--- text_matching_xblock/text_matching_xblock/static/js/src/text_matching_xblock.js
+++ text_matching_xblock/text_matching_xblock/static/js/src/text_matching_xblock.js
@@ -1,21 +1,132 @@
 /* Javascript for TextMatchingXBlock. */
-function TextMatchingXBlock(runtime, element) {
+
+function TextMatchingXBlock(runtime, element, data) {
     function updateCount(result) {
         $('.count', element).text(result.count);
     }
 
-    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+    // Get Handler URL from XBlock runtime
+    let matchOptionUrl = runtime.handlerUrl(element, 'match_option');
+    let swapOptionsUrl = runtime.handlerUrl(element, 'swap_options');
+    let submitUrl = runtime.handlerUrl(element, 'submit');
 
-    $('p', element).click(function(eventObject) {
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({"hello": "world"}),
-            success: updateCount
-        });
-    });
+    // Assign event handler
+    // $('p', element).click(function(eventObject) {
+    //     $.ajax({
+    //         type: "POST",
+    //         url: handlerUrl,
+    //         data: JSON.stringify({"hello": "world"}),
+    //         success: updateCount
+    //     });
+    // });
 
     $(function ($) {
         /* Here's where you'd do things on page load. */
     });
+
+    function swapOptions(first_option_id, second_option_id) {
+
+    }
+
+
+    function matchOption(answerId, optionId) {
+
+        function onUpdateChoiceSuccess(result) {
+
+        }
+
+        function onUpdateChoiceFailed(result) {
+
+        }
+
+        $.ajax({
+            type: "POST",
+            url: matchOptionUrl,
+            data: JSON.stringify({
+                "option_id": optionId,
+                "answer_id": answerId
+            }),
+            success: onUpdateChoiceSuccess
+        });
+    }
+
+    function swapOptions(first_option_id, second_option_id) {
+        function onSwapOptionsSuccess() {
+
+        }
+
+        $.ajax({
+            type: "POST",
+            url: swapOptionsUrl,
+            data: JSON.stringify({
+                "first_option_id": first_option_id,
+                "second_option_id": second_option_id,
+            }),
+            success: onSwapOptionsSuccess
+        });
+    }
+
+    function UniqueDropzone() {
+        let draggable = window.Draggable;
+        console.debug("Init UniqueDropzone Javascript code ")
+        let xblock_id = data.xblock_id
+        console.debug(xblock_id)
+        let containers = document.querySelectorAll(`#${xblock_id} .BlockLayout`);
+
+        if (containers.length === 0) {
+            return false;
+        }
+
+        const droppable = new draggable.Droppable(containers, {
+            draggable: '.Block--isDraggable',
+            dropzone: '.BlockWrapper--isDropzone',
+            mirror: {
+                constrainDimensions: true,
+            },
+        });
+
+        let originZoneId, originZoneType, targetZoneId, targetZoneType;
+
+        // --- Drag start events --- //
+        droppable.on('drag:start', (evt) => {
+            console.debug("Drag starting!!")
+
+            let origin = evt.originalSource.parentNode.dataset
+            originZoneId = origin.dropzoneId;
+            originZoneType = origin.dropzoneType;
+
+        });
+
+        // --- Drop stopped events --- //
+        droppable.on('droppable:dropped', (evt) => {
+            let targetDropzone = evt.dropzone.dataset;
+
+            targetZoneId = targetDropzone.dropzoneId;
+            targetZoneType = targetDropzone.dropzoneType;
+            console.debug(targetZoneId)
+
+            // TODO: Do some validation here
+
+            // Handle student choice
+            if (originZoneType === "Answer" && targetZoneType === "Option") {
+                // Student remove current option
+                console.debug("Student remove current option")
+                matchOption(originZoneId, null)
+            } else if (originZoneType === "Option" && targetZoneType === "Answer") {
+                console.debug("Student select an option")
+                matchOption(targetZoneId, originZoneId)
+            } else if (originZoneType === "Answer" && targetZoneType == "Answer") {
+                // Student try to match the option to another answer
+                // TODO: Find a way to handle this atomically, for now just cancel this type of event
+                console.debug("Student move the option to another answer.")
+                swapOptions(originZoneId, targetZoneId)
+            } else {
+                console.error("Not implement this combination.")
+            }
+        });
+
+        return droppable;
+    }
+
+    UniqueDropzone()
 }
--- text_matching_xblock/text_matching_xblock/text_matching_xblock.py
+++ text_matching_xblock/text_matching_xblock/text_matching_xblock.py
@@ -1,29 +1,114 @@
 """TO-DO: Write a description of what this XBlock is."""
+import dataclasses
+import uuid
+from collections import defaultdict
+
 import pkg_resources
 from web_fragments.fragment import Fragment
 from xblock.core import XBlock
-from xblock.fields import Integer, Scope
+from xblock.fields import Integer, Scope, String, Dict, List, ScopeIds
 
 from text_matching_xblock.utils import render_template
+
+
+@dataclasses.dataclass
+class Dropzone:
+    id: int
+
+
+@dataclasses.dataclass
+class OccupiedDropzone(Dropzone):
+    text: str
+    status: str = "occupied"
+
+
+@dataclasses.dataclass
+class BlankDropzone(Dropzone):
+    status: str = "blank"
+
+
+@dataclasses.dataclass
+class HollowDropzone(Dropzone):
+    text: str
+    status: str = "hollow"
+
 
 class TextMatchingXBlock(XBlock):
     """
     TO-DO: document what your XBlock does.
     """
+    question = String(
+        scope=Scope.content,
+    )
 
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
+    prompts = Dict(
+        default={
+            "1": {
+                "text": "Prompt 1",
+                "id": "1",
+            },
+            "2": {
+                "text": "Prompt 2",
+                "id": "2",
+            },
+            "3": {
+                "text": "Prompt 3",
+                "id": "3",
+            },
+        },
+        scope=Scope.content,
+    )
 
-    # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
+    options = Dict(
+        default={
+            "1": {
+                "text": "Option 1",
+                "id": "1",
+            },
+            "2": {
+                "text": "Option 2",
+                "id": "2",
+            },
+            "3": {
+                "text": "Option 3",
+                "id": "3",
+            },
+        },
+        scope=Scope.content,
+    )
+
+    student_choices = Dict(
+        default={},
+        scope=Scope.user_state,
+        help="A mapping from prompt_id to option_id that has been matched so far."
     )
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
+
+    def _get_xblock_unique_id(self) -> str:
+        """
+        Return unique ID of this block. Useful for HTML ID attributes.
+        Works both in LMS/Studio and workbench runtimes:
+        - In LMS/Studio, use the location.html_id method.
+        - In the workbench, use the usage_id.
+        """
+        # TODO: Comment this below 3 lines since it needs further investigation for its unique
+        # if hasattr(self, 'location'):
+        #     _id = self.location.html_id()
+        # else:
+        _id = self.scope_ids.usage_id
+
+        # This is a hacky way to make block id a valid CSS Selectors
+        # With html_id, "+" and ":" is not a valid CSS Selectors
+        _id = _id.replace("+", "").replace(":", "")
+
+        # For usage_id, "." change CSS Selectors, so it needs to be replaced too.
+        _id = _id.replace(".", "")
+
+        return _id
 
     # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
@@ -35,18 +120,7 @@ class TextMatchingXBlock(XBlock):
 
         html = render_template(
             template_name="text_matching_xblock.html",
-            context={
-                "question": "Some dummy question",
-                # TODO: Replace this dummy id will Xblock id for unique. This is IMPORTANT
-                #  since multiple Matching Xblock can be used in one page.
-                "id": "UniqueDropzone",
-                # TODO: Replace this dummy prompts by real prompt. Currently let's mock this by 3 prompts
-                "prompts": [
-                    "Prompt 1",
-                    "Prompt 2",
-                    "Prompt 3",
-                ],
-            },
+            context=self.prepare_matching_zone_template_context(),
         )
         frag.add_content(html)
 
@@ -68,27 +142,122 @@ class TextMatchingXBlock(XBlock):
         )
 
         frag.add_javascript(self.resource_string("static/js/src/text_matching_xblock.js"))
-        frag.initialize_js('TextMatchingXBlock')
-
-        frag.add_javascript(self.resource_string("static/js/src/drag_and_drop.js"))
+        print(self._get_xblock_unique_id())
+        frag.initialize_js(
+            'TextMatchingXBlock',
+            {
+                "xblock_id": self._get_xblock_unique_id(),
+            },
+        )
 
         return frag
 
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
+    def prepare_matching_zone_template_context(self) -> dict:
+        prompt_context = list(self.prompts.values())
+
+        # Prepare answer context
+        answer_context = []
+        for prompt_id, prompt in self.prompts.items():
+            if option_id := self.student_choices.get(prompt_id):
+                answer_context.append({
+                    "status": "dropzone-occupied",
+                    "id": prompt_id,
+                    "text": self.options[option_id]["text"],
+                })
+            else:
+                answer_context.append({
+                    "status": "blank",
+                    "id": prompt_id,
+                })
+
+        # Prepare option context
+        chosen_option_ids = list(self.student_choices.values())
+        option_context = [
+            {
+                "status": "hollow",
+                "text": option["text"],
+                "id": option["id"],
+            }
+            if option["id"] in chosen_option_ids
+            else
+            {
+                "status": "origin-occupied",
+                "text": option["text"],
+                "id": option["id"],
+            }
+            for option in self.options.values()
+        ]
+
+        print(answer_context)
+        return {
+            "question": "Some dummy question",
+            # TODO: Replace this dummy id will Xblock id for unique. This is IMPORTANT
+            #  since multiple Matching Xblock can be used in one page.
+            "id": self._get_xblock_unique_id(),
+            "prompts": prompt_context,
+            "answers": answer_context,
+            "options": option_context,
+        }
+
     @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
+    def match_option(self, data, suffix=''):
         """
-        An example handler, which increments the data.
+        Match/unmatch an option to an answer
         """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
+        print("match option")
+        print(data)
+        # TODO: Validate request here
+        option_id = data["option_id"]
+        answer_id = data["answer_id"]
+        if not option_id:
+            # Student remove an option
 
-        self.count += 1
-        return {"count": self.count}
+            # Remove option from student choice
+            if not self.student_choices.get(answer_id):
+                raise Exception()
 
-    # TO-DO: change this to create the scenarios you'd like to see in the
-    # workbench while developing your XBlock.
+            del self.student_choices[answer_id]
+        else:
+            # Student match an option to an answer
+            if self.student_choices.get(answer_id):
+                # Can not match an option to occupied answer
+                raise Exception()
+
+            self.student_choices[answer_id] = option_id
+
+        return {"result": "success"}
+
+    @XBlock.json_handler
+    def swap_options(self, data, suffix=''):
+        """
+        Swap 2 options. This method also includes re-match the option to another blank answer
+        """
+        print("Swap option")
+        print(data)
+        # TODO: Validate request here
+        student_choice = self.student_choices
+        first_option_id = data["first_option_id"]
+        second_option_id = data["second_option_id"]
+
+        if not student_choice.get(first_option_id) and not student_choice.get(second_option_id):
+            raise Exception()
+
+        # If one of 2 options is chosen, remove the chosen one and assign that value to the remaining
+        if answer := student_choice.get(first_option_id):
+            student_choice[second_option_id] = answer
+            del student_choice[first_option_id]
+
+        elif answer := student_choice.get(second_option_id):
+            student_choice[first_option_id] = answer
+            del student_choice[second_option_id]
+
+        # If 2 options are chosen, swap those
+        else:
+            # Swap 2 options
+            student_choice[first_option_id], student_choice[second_option_id] = student_choice[second_option_id], student_choice[first_option_id]
+
+        return {"result": "success"}
+
     @staticmethod
     def workbench_scenarios():
         """A canned scenario for display in the workbench."""
